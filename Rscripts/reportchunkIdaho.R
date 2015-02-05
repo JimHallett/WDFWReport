e4e621b014f6b2cd02d7fbc4a9abb8fb4cb80b87
JimHallett <jhallett@ewu.edu>
2015-02-04 21:29:51 -0800

    modified for Idaho

---
+++ Rscripts/reportchunkIdaho.R
@@ -0,0 +1,76 @@
+#####################################################################################################
+## WDFW reporting script for scientific collection permit
+#####################################################################################################
+
+## required libraries
+library(plyr)
+library(tidyr)
+library(dplyr)
+library("RODBC")
+library("lubridate")
+library( ReporteRs )
+
+## pulldata from SQL view
+
+channel <- odbcConnect("discountasp", uid = "SQL2008_508574_uwmep_user", pwd = "Manis9") 
+totmammal <-data.frame(sqlFetch(channel, "Mammal view"))
+close(channel)
+
+## filter and clean the sql dataframe
+
+mammalyear <- totmammal %>%                                  # fetch data from SQL database
+  filter(year(Date)==2014)  %>%                              # set year filter
+  filter(Station == "PR-P2")
+  select(-Owner, -Unit, -Habitat, -Sex:-Year)                  # remove unnecessary columns
+             
+mammalsubset <- mammalyear  %>% 
+  group_by(Station, Northing, Easting, Scientific.Name, Common.Name) %>%
+  summarise(Captures=n())
+
+## rename the columns
+
+names(mammalsubset)<-(c("Station", "Northing", "Easting", "ScientificName", "CommonName", "NumberCaptured"))
+
+## get the date range
+
+daterange <- data.frame(
+  aggregate(Date~Station, data=mammalyear, min),
+  aggregate(Date~Station, data=mammalyear, max))
+daterange <- daterange %>%
+  select(-Station.1)
+colnames(daterange) <- c("Station", "Start", "End")
+
+## merge capture data and locations with date range
+
+finalout <- merge(mammalsubset,daterange,by="Station") %>%
+  select(-Station)  
+
+# Add county and disposition
+
+finalout["County"] <- "Pend Oreille"
+finalout["Disposition"] <- "3"
+
+# Final output
+
+finalout <- finalout[c("Start", "End", "ScientificName", "CommonName", "NumberCaptured", 
+                       "Northing", "Easting", "County", "Disposition")]
+
+# Send to a Word document
+
+reportout = docx()
+reportout = addSection( reportout, landscape = TRUE)
+reportout = addFlexTable(reportout, FlexTable(finalout))
+reportout = addSection( reportout )
+writeDoc( reportout, file = "SCPIdaho2014.docx")
+
+
+
+
+
+
+
+
+
+
+
+
