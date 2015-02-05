be7260b80725d1fa8ab699fbcb8306b9e2d99f2f
JimHallett <jhallett@ewu.edu>
2015-02-05 14:05:53 -0800

    mammals for idaho

--- Rscripts/reportchunkIdaho.R
+++ Rscripts/reportchunkIdaho.R
@@ -20,54 +20,86 @@ close(channel)
 
 mammalyear <- totmammal %>%                                  # fetch data from SQL database
   filter(year(Date)==2014)  %>%                              # set year filter
-  filter(Station == "PR-P2")
-  select(-Owner, -Unit, -Habitat, -Sex:-Year)                  # remove unnecessary columns
-             
-mammalsubset <- mammalyear  %>% 
-  group_by(Station, Northing, Easting, Scientific.Name, Common.Name) %>%
+  filter(Station == "PR-P2") %>%
+  select(-Owner, -Station, -Unit, -Habitat, -Common.Name, -Sex:-Year)                  # remove unnecessary columns
+          
+mammalyear$Day <- day(mammalyear$Date)                       # parse day, month, year
+mammalyear$Month <- month(mammalyear$Date)
+mammalyear$Year <- year(mammalyear$Date)
+
+mammalsubset1 <- mammalyear  %>% 
+  group_by(Northing, Easting, Year, Month, Day, Scientific.Name) %>%
   summarise(Captures=n())
+
+mammalsubset <- select(mammalsubset1, Northing, Easting, Year, Month, Day, Scientific.Name, Captures)
+
+mammalsubset$Observer <- "Permittees"
+mammalsubset$Type <- "Museum Specimen"
+mammalsubset$Confidence <- "Yes"
+mammalsubset$Sex <- "Unknown"
+mammalsubset$Age <- "Adult and Juvenile"
+mammalsubset$CountAcc <- "100%"
+mammalsubset$Location <-"Nonmigratory"
+mammalsubset$Comments <- ""
+mammalsubset$Coord <- "UTM11"
+mammalsubset$Datum <- "NAD27"
+mammalsubset$CoordUnit <- "Meters"
+mammalsubset$Landmark <- ""
+mammalsubset$Owner <- "Tribal"
+
+## rename the columns
+
+mammalsubset <- mammalsubset[c("Observer", "Day", "Month", "Year", "Scientific.Name", "Type", "Confidence", "Sex", "Age",
+                               "Captures", "CountAcc", "Location", "Comments", "Coord", "Datum", "CoordUnit", "Easting", "Northing", "Landmark", "Owner")]
+
+write.csv(mammalsubset, file = "Idahomammals.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ## rename the columns
 
 names(mammalsubset)<-(c("Station", "Northing", "Easting", "ScientificName", "CommonName", "NumberCaptured"))
 
-## get the date range
 
-daterange <- data.frame(
-  aggregate(Date~Station, data=mammalyear, min),
-  aggregate(Date~Station, data=mammalyear, max))
-daterange <- daterange %>%
-  select(-Station.1)
-colnames(daterange) <- c("Station", "Start", "End")
 
-## merge capture data and locations with date range
 
-finalout <- merge(mammalsubset,daterange,by="Station") %>%
-  select(-Station)  
 
-# Add county and disposition
-
-finalout["County"] <- "Pend Oreille"
-finalout["Disposition"] <- "3"
 
 # Final output
 
 finalout <- finalout[c("Start", "End", "ScientificName", "CommonName", "NumberCaptured", 
                        "Northing", "Easting", "County", "Disposition")]
 
-# Send to a Word document
-
-reportout = docx()
-reportout = addSection( reportout, landscape = TRUE)
-reportout = addFlexTable(reportout, FlexTable(finalout))
-reportout = addSection( reportout )
-writeDoc( reportout, file = "SCPIdaho2014.docx")
-
-
-
-
-
-
+# # Send to a Word document
+# 
+# reportout = docx()
+# reportout = addSection( reportout, landscape = TRUE)
+# reportout = addFlexTable(reportout, FlexTable(finalout))
+# reportout = addSection( reportout )
+# writeDoc( reportout, file = "SCPIdaho2014.docx")
+# 
+# 
+# 
+# 
+# 
+# 
 
 
 
